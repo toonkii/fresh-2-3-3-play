8d9adcd2532b842f14b8d5b5aed3e5d3dad3bd91
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-04-29 09:45:28 -0400

    Navigation menu.

---
+++ components/MainNavMenu.tsx
@@ -0,0 +1,38 @@
+export function MainNavMenu() {
+  return (
+    <div class="navbar bg-base-100 shadow-sm">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">Sidereal 365</a>
+      </div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a>Gallery</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul class="bg-base-100 rounded-t-none p-2">
+                <li>
+                  <a>Link 1</a>
+                </li>
+                <li>
+                  <a>Link 2</a>
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li>
+            <a>Contact Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
