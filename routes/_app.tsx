e116574d2899ffade50ea8342431676bf28a4cd0
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-05-03 09:07:47 -0400

    Testing Deno deploy.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -8,7 +8,7 @@ export default define.page(function App({ Component }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>play with DaisyUI</title>
       </head>
-      <body class="container mx-auto h-screen bg-white">
+      <body class="container mx-auto h-screen">
         <Component />
       </body>
     </html>
