14b6f4f14ae0ed8bca50dafa5551920617e15b33
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-05-05 03:10:48 -0400

    Light theme.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -2,7 +2,8 @@ import { define } from "../utils.ts";
 
 export default define.page(function App({ Component }) {
   return (
-    <html data-theme="dark">
+    // <html data-theme="dark">
+    <html data-theme="corporate">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
