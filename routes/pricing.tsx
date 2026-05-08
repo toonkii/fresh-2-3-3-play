22bd7e480e55c75a601c154170741d6738b3fb30
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-05-07 21:45:09 -0400

    Filling in content.

--- routes/pricing.tsx
+++ routes/pricing.tsx
@@ -10,7 +10,7 @@ export default define.page(function PricingFrontPage(_ctx) {
               Pricing
             </h1>
             <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical.
+              Pricing schemes.
             </p>
             {/* <div class="flex mx-auto border-2 border-indigo-500 rounded overflow-hidden mt-6">
               <button class="py-1 px-4 bg-indigo-500 text-white focus:outline-none">
@@ -57,7 +57,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                  </span>?
+                  </span>Start on payment of deposit
                 </p>
                 <p class="flex items-center text-gray-600 mb-6">
                   <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -72,7 +72,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                  </span>Mixtape chillwave tumeric
+                  </span>Payment on completed milestones
                 </p>
                 <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
@@ -104,7 +104,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                 <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
                   <span>BBD$175</span>
                   <span class="text-lg ml-1 font-normal text-gray-500">
-                    /hour
+                    /hr
                   </span>
                 </h1>
                 <p class="flex items-center text-gray-600 mb-2">
@@ -167,7 +167,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                     </svg>
                   </span>Troubleshooting Faults
                 </p>
-                {/* <button class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
+                <button class="flex items-center mt-auto text-white bg-indigo-500 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
                   Button
                   <svg
                     fill="none"
@@ -183,18 +183,18 @@ export default define.page(function PricingFrontPage(_ctx) {
                 </button>
                 <p class="text-xs text-gray-500 mt-3">
                   Literally you probably haven't heard of them jean shorts.
-                </p> */}
+                </p>
               </div>
             </div>
             <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
               <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
                 <h2 class="text-sm tracking-widest title-font mb-1 font-medium">
-                  BUSINESS
+                  ANNUAL BUSINESS SUPPORT
                 </h2>
                 <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                  <span>$56</span>
+                  <span>Annual Fee</span>
                   <span class="text-lg ml-1 font-normal text-gray-500">
-                    /mo
+                    /yr
                   </span>
                 </h1>
                 <p class="flex items-center text-gray-600 mb-2">
@@ -210,54 +210,54 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
+                  </span>Pre pay annual support fee
+                </p>
+                <p class="flex items-center text-gray-600 mb-2">
+                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2.5"
+                      class="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5"></path>
+                    </svg>
+                  </span>Lower hourly rate during the year
+                </p>
+                <p class="flex items-center text-gray-600 mb-2">
+                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2.5"
+                      class="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5"></path>
+                    </svg>
+                  </span>Higher priority
+                </p>
+                {/* <p class="flex items-center text-gray-600 mb-2">
+                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                    <svg
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2.5"
+                      class="w-3 h-3"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M20 6L9 17l-5-5"></path>
+                    </svg>
                   </span>Vexillologist pitchfork
-                </p>
-                <p class="flex items-center text-gray-600 mb-2">
-                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2.5"
-                      class="w-3 h-3"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Tumeric plaid portland
-                </p>
-                <p class="flex items-center text-gray-600 mb-2">
-                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2.5"
-                      class="w-3 h-3"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Hexagon neutra unicorn
-                </p>
-                <p class="flex items-center text-gray-600 mb-2">
-                  <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2.5"
-                      class="w-3 h-3"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M20 6L9 17l-5-5"></path>
-                    </svg>
-                  </span>Vexillologist pitchfork
-                </p>
-                <p class="flex items-center text-gray-600 mb-6">
+                </p> */}
+                {/* <p class="flex items-center text-gray-600 mb-6">
                   <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg
                       fill="none"
@@ -271,7 +271,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>Mixtape chillwave tumeric
-                </p>
+                </p> */}
                 <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
                   <svg
@@ -294,12 +294,12 @@ export default define.page(function PricingFrontPage(_ctx) {
             <div class="p-4 xl:w-1/4 md:w-1/2 w-full">
               <div class="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
                 <h2 class="text-sm tracking-widest title-font mb-1 font-medium">
-                  EMERGENCY
+                  EMERGENCY CALL OUT
                 </h2>
                 <h1 class="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
-                  <span>$72</span>
+                  <span>BBD$250</span>
                   <span class="text-lg ml-1 font-normal text-gray-500">
-                    /mo
+                    /hr
                   </span>
                 </h1>
                 <p class="flex items-center text-gray-600 mb-2">
@@ -315,7 +315,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                  </span>Vexillologist pitchfork
+                  </span>Weekends and holidays
                 </p>
                 <p class="flex items-center text-gray-600 mb-2">
                   <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -330,9 +330,9 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                  </span>Tumeric plaid portland
+                  </span>Postpone ongoing work
                 </p>
-                <p class="flex items-center text-gray-600 mb-2">
+                {/* <p class="flex items-center text-gray-600 mb-2">
                   <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg
                       fill="none"
@@ -345,9 +345,9 @@ export default define.page(function PricingFrontPage(_ctx) {
                     >
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
-                  </span>Hexagon neutra unicorn
-                </p>
-                <p class="flex items-center text-gray-600 mb-2">
+                  </span>?
+                </p> */}
+                {/* <p class="flex items-center text-gray-600 mb-2">
                   <span class="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
                     <svg
                       fill="none"
@@ -376,7 +376,7 @@ export default define.page(function PricingFrontPage(_ctx) {
                       <path d="M20 6L9 17l-5-5"></path>
                     </svg>
                   </span>Mixtape chillwave tumeric
-                </p>
+                </p> */}
                 <button class="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">
                   Button
                   <svg
