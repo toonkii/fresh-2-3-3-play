7c2c50d6c2cc7c753d59c98bcb167ec1a1472e90
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-05-17 00:59:44 -0400

    All rights reserved.

--- components/DarkFooter2.tsx
+++ components/DarkFooter2.tsx
@@ -5,7 +5,7 @@ export function DarkFooter2() {
       <footer class="bg-slate-100 dark:bg-neutral-900 py-6 px-4 md:px-8">
         <div class="flex justify-between items-center flex-col lg:flex-row flex-wrap gap-6 text-center">
           <p class="text-sm text-slate-600 dark:text-slate-300">
-            © ReadymadeUI. All rights reserved.
+            © Sidereal 365 Inc. All rights reserved.
           </p>
 
           <ul class="flex space-x-6 gap-y-4 max-lg:justify-center flex-wrap text-sm text-slate-600 dark:text-slate-300">
@@ -25,14 +25,14 @@ export function DarkFooter2() {
                 Privacy Policy
               </a>
             </li>
-            <li>
+            {/* <li>
               <a
                 href="#"
                 class="hover:text-slate-900 dark:hover:text-slate-50 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 rounded"
               >
                 Contact
               </a>
-            </li>
+            </li> */}
           </ul>
         </div>
       </footer>
