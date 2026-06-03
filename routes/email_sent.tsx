1faeaef8017c6f6ac0c06ca9aaa8b2d8ba53d3dd
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-06-02 21:46:30 -0400

    Production version.

---
+++ routes/email_sent.tsx
@@ -0,0 +1,12 @@
+import { define } from "../utils.ts";
+
+export default define.page(function EmailSentSuccesfullyPage(_ctx) {
+    console.log(_ctx);
+  return (
+    <>
+      <h1 class="text-2xl/7 font-bold sm:truncate sm:text-3xl sm:tracking-tight">
+        Email Succesfully Sent
+      </h1>
+    </>
+  );
+});
