9da66106d476aeff8f6e11d50a6b71a5e759b192
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-06-02 21:45:32 -0400

    Email sent basic page.

--- routes/contact.tsx
+++ routes/contact.tsx
@@ -1,27 +1,67 @@
 import { define } from "../utils.ts";
+// import { Handlers } from "$fresh/server.ts";
 
-export default define.page(function ContactPage(_) {
+export const handler = define.handlers({
+  async GET(ctx) {
+    console.log("req", await ctx.req);
+    return ctx;
+  },
+
+  // async POST(ctx) {
+  //   const form = await ctx.req.formData();
+
+  //   console.log("req in post", ctx.req);
+  //   const email = form.get("email")?.toString();
+  //   console.log("email", email);
+  //   console.log("form", form);
+
+  //   // Add email to list.
+
+  //   // Redirect user to thank you page.
+  //   const headers = new Headers();
+  //   headers.set("location", "http://localhost:5173/");
+
+  //   return new Response(null, {
+  //     status: 303, // redirect
+  //     headers,
+  //   });
+  // },
+});
+
+export default define.page<typeof handler>(function ContactPage(_) {
   return (
     <>
-      {/* <h1 class="text-2xl/7 font-bold sm:truncate sm:text-3xl sm:tracking-tight">
+      <form
+        action="http://localhost:8888/email"
+        method="post"
+        enctype="application/x-www-form-urlencoded"
+      >
+        {/* <form action="http://localhost:8888/email" method="post" enctype="application/json"> */}
+        {/* <form action="http://localhost:8888/email" method="post"> */}
+        {/* <form action="https://resend.cyberseainc.com/email" method="post"> */}
+
+        {
+          /* <h1 class="text-2xl/7 font-bold sm:truncate sm:text-3xl sm:tracking-tight">
         Contact Us
-      </h1> */}
+      </h1> */
+        }
 
-      <section class="px-4 md:px-8 mt-6">
-        <div class="max-w-4xl mx-auto">
-          <div class="max-w-3xl mb-12 md:mb-16">
-            <h2 class="text-3xl font-bold text-slate-900 mb-6 md:text-4xl">
-              Contact us
-            </h2>
-            <p class="text-base leading-relaxed text-slate-600 text-lg">
-              Have a question, need support, or want to discuss your next
-              project? We’re here to help.
-            </p>
-          </div>
+        <section class="px-4 md:px-8 mt-6">
+          <div class="max-w-4xl mx-auto">
+            <div class="max-w-3xl mb-12 md:mb-16">
+              <h2 class="text-3xl font-bold text-slate-900 mb-6 md:text-4xl">
+                Contact us
+              </h2>
+              <p class="text-base leading-relaxed text-slate-600 text-lg">
+                Have a question, need support, or want to discuss your next
+                project? We’re here to help.
+              </p>
+            </div>
 
-          <div class="space-y-12">
-            <div class="grid gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4">
-              {/* <div class="flex items-start gap-4">
+            <div class="space-y-12">
+              <div class="grid gap-x-6 gap-y-8 sm:grid-cols-2 lg:grid-cols-4">
+                {
+                  /* <div class="flex items-start gap-4">
                 <div class="shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -49,57 +89,59 @@ export default define.page(function ContactPage(_) {
                     123 Main Street, City, Country
                   </p>
                 </div>
-              </div> */}
+              </div> */
+                }
 
-              <div class="flex items-start gap-4">
-                <div class="shrink-0">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="size-6 fill-slate-600"
-                    viewBox="0 0 32 32"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M22.56 30a5.2 5.2 0 0 1-2-.41A34.53 34.53 0 0 1 2.4 11.42a5 5 0 0 1 1.06-5.51l3-3a3 3 0 0 1 4.24 0l3.53 3.53a3 3 0 0 1 0 4.24l-1.63 1.65a12.54 12.54 0 0 0 7.07 7.07l1.68-1.67a3 3 0 0 1 4.24 0l3.53 3.53a3 3 0 0 1 0 4.24l-3 3a5 5 0 0 1-3.56 1.5M8.62 4a1 1 0 0 0-.71.29l-3 3a3 3 0 0 0-.64 3.31 32.47 32.47 0 0 0 17.1 17.16 3 3 0 0 0 3.31-.64l3-3a1 1 0 0 0 0-1.42l-3.54-3.53a1 1 0 0 0-1.41 0l-2.12 2.12a1 1 0 0 1-1 .24 14.42 14.42 0 0 1-9.12-9.12 1 1 0 0 1 .24-1l2.12-2.12a1 1 0 0 0 .29-.71 1 1 0 0 0-.29-.7L9.33 4.29A1 1 0 0 0 8.62 4"
-                      data-original="#000000"
+                <div class="flex items-start gap-4">
+                  <div class="shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="size-6 fill-slate-600"
+                      viewBox="0 0 32 32"
+                      aria-hidden="true"
                     >
-                    </path>
-                  </svg>
+                      <path
+                        d="M22.56 30a5.2 5.2 0 0 1-2-.41A34.53 34.53 0 0 1 2.4 11.42a5 5 0 0 1 1.06-5.51l3-3a3 3 0 0 1 4.24 0l3.53 3.53a3 3 0 0 1 0 4.24l-1.63 1.65a12.54 12.54 0 0 0 7.07 7.07l1.68-1.67a3 3 0 0 1 4.24 0l3.53 3.53a3 3 0 0 1 0 4.24l-3 3a5 5 0 0 1-3.56 1.5M8.62 4a1 1 0 0 0-.71.29l-3 3a3 3 0 0 0-.64 3.31 32.47 32.47 0 0 0 17.1 17.16 3 3 0 0 0 3.31-.64l3-3a1 1 0 0 0 0-1.42l-3.54-3.53a1 1 0 0 0-1.41 0l-2.12 2.12a1 1 0 0 1-1 .24 14.42 14.42 0 0 1-9.12-9.12 1 1 0 0 1 .24-1l2.12-2.12a1 1 0 0 0 .29-.71 1 1 0 0 0-.29-.7L9.33 4.29A1 1 0 0 0 8.62 4"
+                        data-original="#000000"
+                      >
+                      </path>
+                    </svg>
+                  </div>
+                  <div>
+                    <h3 class="text-slate-900 text-lg font-semibold">
+                      Call us
+                    </h3>
+                    <p class="text-lg text-slate-600 mt-1">246-234-9692</p>
+                  </div>
                 </div>
-                <div>
-                  <h3 class="text-slate-900 text-lg font-semibold">
-                    Call us
-                  </h3>
-                  <p class="text-lg text-slate-600 mt-1">246-234-9692</p>
-                </div>
-              </div>
 
-              <div class="flex items-start gap-4">
-                <div class="shrink-0">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="size-6 fill-slate-600"
-                    viewBox="0 0 24 24"
-                    aria-hidden="true"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M.41 4.747A4.35 4.35 0 0 1 4.76.4h14.488a4.35 4.35 0 0 1 4.35 4.352l-.007 10.109a4.35 4.35 0 0 1-4.35 4.346H13.52a.3.3 0 0 0-.188.07l-4.548 3.84c-1.319 1.113-3.338.176-3.338-1.552v-2.068a.29.29 0 0 0-.29-.29h-.403a4.35 4.35 0 0 1-4.35-4.352zM4.76 2.14a2.61 2.61 0 0 0-2.61 2.608l-.008 10.108a2.61 2.61 0 0 0 2.61 2.611h.403c1.12 0 2.03.91 2.03 2.03v2.068a.29.29 0 0 0 .475.22l4.548-3.839a2.03 2.03 0 0 1 1.31-.479h5.723a2.61 2.61 0 0 0 2.61-2.608l.007-10.108a2.61 2.61 0 0 0-2.61-2.61zm2.128 5.29a.87.87 0 0 1 .87-.87h8.485a.87.87 0 0 1 0 1.74H7.757a.87.87 0 0 1-.87-.87zm0 4.744a.87.87 0 0 1 .87-.87h4.781a.87.87 0 0 1 0 1.74H7.758a.87.87 0 0 1-.87-.87"
-                      clip-rule="evenodd"
-                      data-original="#000000"
+                <div class="flex items-start gap-4">
+                  <div class="shrink-0">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="size-6 fill-slate-600"
+                      viewBox="0 0 24 24"
+                      aria-hidden="true"
                     >
-                    </path>
-                  </svg>
+                      <path
+                        fill-rule="evenodd"
+                        d="M.41 4.747A4.35 4.35 0 0 1 4.76.4h14.488a4.35 4.35 0 0 1 4.35 4.352l-.007 10.109a4.35 4.35 0 0 1-4.35 4.346H13.52a.3.3 0 0 0-.188.07l-4.548 3.84c-1.319 1.113-3.338.176-3.338-1.552v-2.068a.29.29 0 0 0-.29-.29h-.403a4.35 4.35 0 0 1-4.35-4.352zM4.76 2.14a2.61 2.61 0 0 0-2.61 2.608l-.008 10.108a2.61 2.61 0 0 0 2.61 2.611h.403c1.12 0 2.03.91 2.03 2.03v2.068a.29.29 0 0 0 .475.22l4.548-3.839a2.03 2.03 0 0 1 1.31-.479h5.723a2.61 2.61 0 0 0 2.61-2.608l.007-10.108a2.61 2.61 0 0 0-2.61-2.61zm2.128 5.29a.87.87 0 0 1 .87-.87h8.485a.87.87 0 0 1 0 1.74H7.757a.87.87 0 0 1-.87-.87zm0 4.744a.87.87 0 0 1 .87-.87h4.781a.87.87 0 0 1 0 1.74H7.758a.87.87 0 0 1-.87-.87"
+                        clip-rule="evenodd"
+                        data-original="#000000"
+                      >
+                      </path>
+                    </svg>
+                  </div>
+                  <div>
+                    <h3 class="text-slate-900 text-lg font-semibold">
+                      Chat to us
+                    </h3>
+                    <p class="text-lg text-slate-600 mt-1">Use email form.</p>
+                  </div>
                 </div>
-                <div>
-                  <h3 class="text-slate-900 text-lg font-semibold">
-                    Chat to us
-                  </h3>
-                  <p class="text-lg text-slate-600 mt-1">Use email form.</p>
-                </div>
-              </div>
 
-              {/* <div class="flex items-start gap-4">
+                {
+                  /* <div class="flex items-start gap-4">
                 <div class="shrink-0">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -118,102 +160,104 @@ export default define.page(function ContactPage(_) {
                   <h3 class="text-slate-900 text-base font-semibold">Fax</h3>
                   <p class="text-lg text-slate-600 mt-1">+1-548-2588</p>
                 </div>
-              </div> */}
+              </div> */
+                }
+              </div>
+
+              <form class="space-y-6 max-w-xl">
+                <div>
+                  <label
+                    for="name"
+                    class="mb-2 text-slate-900 font-medium text-lg inline-block"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="John Doe"
+                    required
+                    class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+                <div>
+                  <label
+                    for="email"
+                    class="mb-2 text-slate-900 font-medium text-lg inline-block"
+                  >
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="john.doe@example.com"
+                    required
+                    class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+                <div>
+                  <label
+                    for="phone"
+                    class="mb-2 text-slate-900 font-medium text-lg inline-block"
+                  >
+                    Phone number
+                  </label>
+                  <input
+                    type="number"
+                    id="phone"
+                    name="phone"
+                    placeholder="259-8543"
+                    required
+                    class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+                <div>
+                  <label
+                    for="subject"
+                    class="mb-2 text-slate-900 font-medium text-lg inline-block"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    placeholder="XYZ Inc."
+                    class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  />
+                </div>
+                <div>
+                  <label
+                    for="message"
+                    class="mb-2 text-slate-900 font-medium text-lg inline-block"
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    placeholder="Write message"
+                    rows="6"
+                    type="text"
+                    id="text"
+                    name="text"
+                    required
+                    class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
+                  >
+                  </textarea>
+                </div>
+
+                <button
+                  type="submit"
+                  class="py-2.5 px-4 text-lg rounded-md font-semibold cursor-pointer text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
+                >
+                  Send Message
+                </button>
+              </form>
             </div>
-
-            <form class="space-y-6 max-w-xl">
-              <div>
-                <label
-                  for="name"
-                  class="mb-2 text-slate-900 font-medium text-lg inline-block"
-                >
-                  Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="John Doe"
-                  required
-                  class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
-                />
-              </div>
-              <div>
-                <label
-                  for="email"
-                  class="mb-2 text-slate-900 font-medium text-lg inline-block"
-                >
-                  Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="john.doe@example.com"
-                  required
-                  class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
-                />
-              </div>
-              <div>
-                <label
-                  for="phone"
-                  class="mb-2 text-slate-900 font-medium text-lg inline-block"
-                >
-                  Phone number
-                </label>
-                <input
-                  type="number"
-                  id="phone"
-                  name="phone"
-                  placeholder="259-8543"
-                  required
-                  class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
-                />
-              </div>
-              <div>
-                <label
-                  for="company"
-                  class="mb-2 text-slate-900 font-medium text-lg inline-block"
-                >
-                  Company
-                </label>
-                <input
-                  type="text"
-                  id="company"
-                  name="company"
-                  placeholder="XYZ Inc."
-                  class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
-                />
-              </div>
-              <div>
-                <label
-                  for="message"
-                  class="mb-2 text-slate-900 font-medium text-lg inline-block"
-                >
-                  Message
-                </label>
-                <textarea
-                  placeholder="Write message"
-                  rows="6"
-                  type="text"
-                  id="message"
-                  name="message"
-                  required
-                  class="px-3 py-2.5 text-lg text-slate-900 w-full rounded-md bg-white outline-1 -outline-offset-1 outline-slate-400 focus:outline-2 focus:-outline-offset-2 focus:outline-blue-600"
-                >
-                </textarea>
-              </div>
-
-              <button
-                type="submit"
-                class="py-2.5 px-4 text-lg rounded-md font-semibold cursor-pointer text-white border border-blue-600 bg-blue-600 hover:bg-blue-700 transition-all focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500"
-              >
-                Send Message
-              </button>
-            </form>
           </div>
-        </div>
-      </section>
+        </section>
+      </form>
     </>
   );
 });
