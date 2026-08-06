203820ac617aad6dcab8858ce3971182fd5a16fb
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-08-06 02:52:44 -0400

    Removed dotenv.

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,10 +2,10 @@
 import { UnderConstruction } from "@/components/UnderConstruction.tsx";
 import { define } from "../utils.ts";
 import { Landing } from "@/components/Landing.tsx";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
 export default define.page(async function LandingPage(_ctx) {
-  dotenv.config({ override: true });
+  // dotenv.config({ override: true });
   const mode = Deno.env.get("MODE");
 
   return (
