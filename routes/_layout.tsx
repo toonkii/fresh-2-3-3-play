3a172a3a80a5c1bf71af92b3ace69ad58529ac9a
Thomas Clarke <thomas.p.clarke@gmail.com>
2026-08-06 03:03:26 -0400

    Using dotenv.

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,11 +1,11 @@
 import { PageProps } from "fresh";
 import { MainNavMenu } from "../components/MainNavMenu.tsx";
 import { DarkFooter2 } from "@/components/DarkFooter2.tsx";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 
 export default function MainLayout({ Component }: PageProps) {
   // Force reload and overwrite process.env
-  // dotenv.config({ override: true });
+  dotenv.config({ override: true });
 
   const mode = Deno.env.get("MODE");
   // console.log("mode:", mode);
